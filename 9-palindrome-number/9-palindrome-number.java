69ed521b2d440e778eb465f3a7102bfbbf2488cc
lianghongtruly <p.lianghong@gmail.com>
2022-07-10 21:40:46 +0800

    Time: 372 ms (5.12%), Space: 53.6 MB (5.28%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int temp = x; 
-        int remainder;
-        int sum = 0;//used to store reversed
-
-        while(x > 0){
-            remainder = x % 10;
-            sum = (sum * 10 ) + remainder;
-            x = x/10;
-            System.out.println(sum);
-        }
-        if(sum == temp)
-            return true;
-        else
+        int reversed = 0;
+        if(x < 0 || (x % 10 == 0 && x != 0)){
             return false;
+        }
+        while(x > reversed){
+            reversed = reversed * 10 + x % 10;
+            x = x / 10;
+            System.out.println("reversed: " + reversed);
+            System.out.println("x: " + x);
+
+        }
+        return (x == reversed || x == reversed / 10);
     }
 }
