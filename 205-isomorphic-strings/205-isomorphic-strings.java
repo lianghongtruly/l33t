1174eed2832a67322fa39d8f8b5d83f6bef667de
lianghongtruly <p.lianghong@gmail.com>
2022-08-12 15:27:28 +0800

    Time: 7 ms (83.58%), Space: 42.5 MB (82.58%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -3,8 +3,10 @@ class Solution {
         int[] ss = new int[256]; 
         int[] tt = new int[256];
         for(int i = 0; i < s.length(); ++i) {
-            if(ss[s.charAt(i)] != tt[t.charAt(i)]) return false;
-            ss[s.charAt(i)] = tt[t.charAt(i)] = i+1;
+            if(ss[s.charAt(i)] != tt[t.charAt(i)]) 
+                return false;
+            ss[s.charAt(i)] = i + 1;
+            tt[t.charAt(i)] = i + 1;
         }
         return true;
     }
