7ac92a4405c7274ad7fb3664a251d9d4faad2e19
lianghongtruly <p.lianghong@gmail.com>
2022-08-18 21:19:19 +0800

    Time: 1 ms (99.83%), Space: 43.6 MB (75.50%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,29 +1,48 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        ArrayList<Integer> result = new ArrayList<>();
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int i = 0;
-        int j = 0;
+//         ArrayList<Integer> result = new ArrayList<>();
+//         Arrays.sort(nums1);
+//         Arrays.sort(nums2);
+//         int i = 0;
+//         int j = 0;
         
-        while(i < nums1.length && j < nums2.length){
-            if(nums1[i] > nums2[j])
-                j++;
-            else if(nums1[i] < nums2[j])
-                i++;
-            else{
-                result.add(nums1[i]);
-                i++;
-                j++;
+//         while(i < nums1.length && j < nums2.length){
+//             if(nums1[i] > nums2[j])
+//                 j++;
+//             else if(nums1[i] < nums2[j])
+//                 i++;
+//             else{
+//                 result.add(nums1[i]);
+//                 i++;
+//                 j++;
+//             }
+//         }
+        
+//         int[] res = new int[result.size()];
+//         for(i = 0; i < result.size(); i++){
+//             res[i] = (int) result.get(i);
+//         }
+        
+//         return res;
+        
+        if(nums1.length > nums2.length)
+            return intersect(nums2, nums1);
+        
+        int[] foreplay = new int[1001];
+        for(int n: nums1)
+            foreplay[n]++;
+        
+        int[] answer = new int[nums1.length];
+        
+        int i = 0;
+        
+        for(int n: nums2) {
+            if(foreplay[n] != 0){
+                answer[i++] = n;
+                foreplay[n]--;
             }
         }
-        
-        int[] res = new int[result.size()];
-        for(i = 0; i < result.size(); i++){
-            res[i] = (int) result.get(i);
-        }
-        
-        return res;
+        return Arrays.copyOf(answer, i);
         
     }
 }
