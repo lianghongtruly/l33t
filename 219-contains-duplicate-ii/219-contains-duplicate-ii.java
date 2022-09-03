7b04ffa0dfc8dca4a65bc66699a9bab763e6deab
lianghongtruly <p.lianghong@gmail.com>
2022-09-03 16:43:20 +0800

    Time: 18 ms (97.25%), Space: 50.1 MB (99.11%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.java
@@ -14,9 +14,9 @@ class Solution {
         for(int i=0; i<nums.length; i++) {
             if(map.containsKey(nums[i]) && (i-map.get(nums[i]))<=k) {
                 return true;
-            } else {
-                map.put(nums[i], i);
             }
+            map.put(nums[i], i);
+            
         }
         return false;
     }
