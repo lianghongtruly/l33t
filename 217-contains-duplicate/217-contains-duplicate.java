a1edbad2c731419e0fbcce47e75f397ac71051d3
lianghongtruly <p.lianghong@gmail.com>
2022-09-03 15:09:15 +0800

    Time: 6 ms (97.38%), Space: 71.6 MB (19.84%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        for (int i=0; i<nums.length-1; i++){
+            for (int j=i+1; j>0; j--){
+                if (nums[j]<nums[j-1]){
+                    
+                    int t = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = t;
+                }
+                else if(nums[j]==nums[j-1]){
+                    return true;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        
+        return false;
+        
+    }
+}
